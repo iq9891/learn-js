60da36cf9a4bfd3c5affefe6ce01b607a0e94491
李梦龙 <649076408@qq.com>
2018-03-29 16:58:22 +0800

    feature: 01 - JavaScript Drum Kit

--- README.md
+++ README.md
@@ -1,13 +1,2 @@
-# learn-js
-博客中js相关的案例代码
-
-## 条件
-
-1. node 6.0.0+, npm 3.8.6+
-2. git
-
-## run
-
-`npm i`
-
-`npm start` 运行
+# learn-js - native 分支
+> JavaScript 原生的各种练习。
--- gulpfile.babel.js
+++
@@ -1,8 +0,0 @@
-import gulp from 'gulp';
-import babel from 'gulp-babel';
-
-gulp.task('default', ()=>{
-    return gulp.src('js/**/*.js')
-        .pipe(babel())
-        .pipe(gulp.dest('dist'));
-});
