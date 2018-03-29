3816b4cd90a66208b2e4ed6c4c3db2c1d662bb63
李梦龙 <649076408@qq.com>
2018-03-29 20:34:37 +0800

    feature: 04 - Array Cardio Day 1

---
+++ 04 - Array Cardio Day 1/README.md
@@ -0,0 +1,19 @@
+# 04 - 数组练习1
+
+## 目录结构
+``` bash
+├── index.html                   # 页面
+└── script.js                    # 写 JavaScript 的地方
+```
+
+## 功能
+1. 筛选 16 世纪出生的人(inventors 中 year 在 1501-1600)
+2. 给我们一组发明家( inventors )的名字(第一个名字( first ) + 第二个名字( last ))
+3. 通过出生日期发明人( inventors )排序，大到小
+4. 所有发明家( inventors )共活了多少年？
+5. 按岁数把发明家( inventors )排序
+6. 数组 people 按姓氏字母顺序排序 ( last )
+7. 写一个求和( sum )方法，计算重复的单词出现几次
+
+## 时间
+建议用时 2 小时
---
+++ 04 - Array Cardio Day 1/script.js
@@ -0,0 +1,47 @@
+// first 姓
+// last 名
+// year 出生日期
+// passed 去世日期
+const inventors = [
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+  { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+  { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+];
+
+const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
+
+// 1. 筛选 16 世纪出生的人(inventors 中 year 在 1501-1600)
+
+
+// 2. 给我们一组发明家( inventors )的名字(第一个名字( first ) + 第二个名字( last ))
+
+
+// 3. 通过出生日期发明人( inventors )排序，大到小
+
+
+// 4. 所有发明家( inventors )共活了多少年？
+
+
+// 5. 按岁数把发明家( inventors )排序
+
+
+// 6. 数组 people 按姓氏字母顺序排序 ( last )
+
+
+// 7. 写一个求和( sum )方法，计算重复的单词出现几次
+// 输出格式 {bike : 2,car : 5,truck : 3,van: 2,walk: 2,}
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+var sum = function(arr) {
+
+};
+
+console.table(sum(data));
